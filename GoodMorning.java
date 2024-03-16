065254e2e29b3b423353c20b14210bc56f827149
chen shahaf <48526807+ChenShahaf@users.noreply.github.com>
2024-03-16 12:24:56 +0200

    Answer to the question Good Morning
    open the source code to see solutions

---
+++ GoodMorning.java
@@ -0,0 +1,31 @@
+
+/* Write a program that contain a class named: GoodMorning
+ *  the program should print out to the screen Good Morning, people
+ *
+ * please write your answer
+ * 
+ * */
+ 
+//Author Chen Shahaf 
+package GoodMorning;
+
+public class GoodMorning {
+	
+	public static void main(String[] args) {
+		
+		//OPETION  1 - Creating a method to print the requested msg
+		GoodMorningPrint();		
+		
+		//Option 2 - directly print the phrase
+		//System.out.println("Good Morning, people");
+		
+		
+		
+		
+	}
+	
+	public static void GoodMorningPrint() {
+		System.out.println("Good Morning, people");
+	}
+
+}
